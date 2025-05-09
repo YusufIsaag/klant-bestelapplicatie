c0d9ea940f166d3cbe4864bbd4d73ad7ae100b53
YusufIsaag <2404344isaag@zuyd.nl>
2025-05-09 20:03:43 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+"use strict";
+document.querySelector('.winkelwagen-knop').addEventListener('click', function() {
+    alert("Winkelwagen geopend!"); // Test of het werkt
+    // window.location.href = "/winkelwagen"; // Gebruik dit voor echte navigatie
+});
+
+"use strict";
+document.querySelector('.item4').addEventListener('click', function() {
+    alert("Thuis pagina wordt geladen!"); // Test of het werkt
+    // window.location.href = "/winkelwagen"; // Gebruik dit voor echte navigatie
+});
+
+"use strict";
+document.querySelector('.item5').addEventListener('click', function() {
+    alert("producten pagina wordt geladen!"); // Test of het werkt
+    // window.location.href = "/winkelwagen"; // Gebruik dit voor echte navigatie
+});
